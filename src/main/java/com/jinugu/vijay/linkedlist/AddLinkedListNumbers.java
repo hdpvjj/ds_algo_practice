537defcc54a3362001950ad4f3618337a1f42968
Vijay <vijay.jinugu@gmail.com>
2018-12-16 11:21:11 -0500

    LinkedList-Add two numbers

---
+++ src/main/java/com/jinugu/vijay/linkedlist/AddLinkedListNumbers.java
@@ -0,0 +1,45 @@
+package com.jinugu.vijay.linkedlist;
+
+public class AddLinkedListNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = null;
+        if (l1 == null && l2 == null) {
+            System.out.println("Lists are empty");
+        }
+        if (l1 == null && l2 != null) {
+            return l2;
+        }
+        if(l1 != null && l2 == null){
+            return l1;
+        }
+        int n1;
+        int n2;
+        int r;
+        ListNode prev = null;
+        int carry = 0;
+        while(l1 != null || l2 != null){
+            n1 = l1 != null ? l1.val : 0;
+            n2 = l2 != null ? l2.val : 0;
+            r = n1 + n2 + carry;
+            carry = (r / 10);
+            r = r % 10;
+            ListNode node = new ListNode(r);
+            if(result == null)
+                result = node;
+            else
+                prev.next = node;
+            prev = node;
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+        }
+        if(carry > 0){
+            ListNode node = new ListNode(carry);
+            prev.next = node;
+        }
+        return result;
+    }
+
+    public static void main(String args[]){
+        System.out.println("Start");
+    }
+}
